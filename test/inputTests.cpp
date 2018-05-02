787d404e488650cf1089a353cdcc2650f210523e
MaxVanHoucke <max.vanhoucke@student.uantwerpen.be>
2018-05-02 22:22:55 +0200

    Input test happyDay

--- test/inputTests.cpp
+++ test/inputTests.cpp
@@ -19,11 +19,60 @@ protected:
 TEST_F(inputTest, HappyDay) {
     ASSERT_TRUE(FileExists("../test/testInput/happyDay.xml"));
     input.read("../test/testInput/happyDay.xml");
+
     vector<Flightplan*> flightplans = input.getFlightplans();
+    EXPECT_EQ(flightplans.size(), size_t(1));
+
+    vector<Airport*> airports = input.getAirports();
+    EXPECT_EQ(airports.size(), size_t(1));
+
+    Flightplan* flightplan = flightplans[0];
+    EXPECT_TRUE(flightplan != NULL);
+
     Airport* airport = input.getAirports()[0];
-    ASSERT_TRUE(airport != NULL);
+    EXPECT_TRUE(airport != NULL);
 
     vector<Runway*> runways = airport->getRunways();
+    EXPECT_EQ(runways.size(), size_t(2));
+
+    // Airport
+    EXPECT_EQ(airport->getName(), "Antwerp International Airport");
+    EXPECT_EQ(airport->getIata(), "ANR");
+    EXPECT_EQ(airport->getCallsign(), "Antwerp Tower");
+    EXPECT_EQ(airport->getGates(), 10);
+
+    // Runways
+    Runway* runway1 = runways[0];
+    EXPECT_EQ(runway1->getName(), "11R");
+    EXPECT_EQ(runway1->getAirport(), airport);
+    EXPECT_EQ(runway1->getType(), kAsphalt);
+    EXPECT_EQ(runway1->getLength(), 1510);
+    EXPECT_EQ(runway1->getTaxiPoint(), "Alpha");
+
+    Runway* runway2 = runways[1];
+    EXPECT_EQ(runway2->getName(), "11L");
+    EXPECT_EQ(runway2->getAirport(), airport);
+    EXPECT_EQ(runway2->getType(), kGrass);
+    EXPECT_EQ(runway2->getLength(), 1010);
+    EXPECT_EQ(runway2->getTaxiPoint(), "Bravo");
+
+    // Flightplan
+    EXPECT_EQ(flightplan->getDestination(), "LCY");
+    EXPECT_EQ(flightplan->getDeparture(), 30);
+    EXPECT_EQ(flightplan->getArrival(), 15);
+    EXPECT_EQ(flightplan->getInterval(), 3);
+
+    Airplane* airplane = flightplan->getAirplane();
+    EXPECT_TRUE(airplane != NULL);
+
+    EXPECT_EQ(airplane->getNumber(), "N11842");
+    EXPECT_EQ(airplane->getCallsign(), "Cessna 842");
+    EXPECT_EQ(airplane->getModel(), "Cessna 340");
+    EXPECT_EQ(airplane->getType(), kPrivate);
+    EXPECT_EQ(airplane->getEngine(), kJet);
+    EXPECT_EQ(airplane->getSize(), kSmall);
+    EXPECT_EQ(airplane->getFuel(), 10);
+    EXPECT_EQ(airplane->getPassengers(), 8);
 }
 
 TEST_F(inputTest, legalInput) {
